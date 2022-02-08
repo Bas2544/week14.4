25fb5e62f33729b2cea3dccf8a42a90533c8bccd
Bas2544 <thebasza.2544@gmail.com>
2022-02-08 14:16:25 +0700

    first2

--- src/main/java/nvc/it/springapi/controller/ProductController.java
+++ src/main/java/nvc/it/springapi/controller/ProductController.java
@@ -6,12 +6,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import nvc.it.springapi.model.Product;
+import nvc.it.springapi.model.Review;
 import nvc.it.springapi.service.ProductSevice;
 
 @RestController
@@ -42,7 +48,44 @@ public class ProductController {
     public ResponseEntity<Object> getProductByName(@PathVariable String name){
         HashMap<String, Object> map = new HashMap<>();
         map.put("msg", "Search By Name Success!");
-        map.put("data", productSevice.findById(name));
+        map.put("data", productSevice.findProductByName(name));
         return new ResponseEntity<Object>(map,HttpStatus.OK);
     }
+
+    @PostMapping("")
+    public ResponseEntity<Object> addProduct(@RequestBody Product product){
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("msg", "Add Product Success!");
+        map.put("data", productSevice.addProduct(product));
+        return new ResponseEntity<Object>(map,HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateProduct(@PathVariable String id, @RequestBody Product product){
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("msg", "Update Product Success!");
+        map.put("data", productSevice.updateProduct(id, product));
+        return new ResponseEntity<Object>(map,HttpStatus.OK);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Object> addReview(@PathVariable String id, @RequestBody Review review){
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("msg", "Update Product Success!");
+        map.put("data", productSevice.addReview(id, review));
+        return new ResponseEntity<Object>(map,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable String id){
+        HashMap<String, Object> map = new HashMap<>();
+        if(!productSevice.deleteProduct(id)){
+            map.put("msg", "Error delete!");
+            return new ResponseEntity<Object>(map, HttpStatus.INTERNAL_SERVER_ERROR);
+        }else{
+            map.put("msg", "Delete success!");
+            return new ResponseEntity<Object>(map, HttpStatus.OK);
+        }
+    }
+
 }
